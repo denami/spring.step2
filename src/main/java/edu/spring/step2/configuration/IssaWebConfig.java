3902144356750bf805cee4446c3c4eaca7768a40
Andrei Laptsinski <Andrei_Laptsinski@epam.com>
2018-03-16 17:15:31 +0300

    EDU-004 Add Spring form. ModelAndView

--- src/main/java/edu/spring/step2/configuration/IssaWebConfig.java
+++ src/main/java/edu/spring/step2/configuration/IssaWebConfig.java
@@ -1,28 +1,20 @@
 package edu.spring.step2.configuration;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-import org.springframework.web.servlet.view.InternalResourceViewResolver;
-import org.springframework.web.servlet.view.JstlView;
 
 @Configuration
 @ComponentScan(basePackages = "edu.spring.step2")
 @EnableWebMvc
 public class IssaWebConfig implements WebMvcConfigurer {
 
-    @Bean
-    public ViewResolver viewResolver() {
-        InternalResourceViewResolver  viewResolver = new InternalResourceViewResolver();
-        viewResolver.setViewClass(JstlView.class);
-        viewResolver.setPrefix("/WEB-INF/pages/");
-        viewResolver.setSuffix(".jsp");
-
-        return viewResolver;
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.jsp("/WEB-INF/pages/", ".jsp");
     }
 
     @Override
--- src/main/java/edu/spring/step2/controller/UserController.java
+++ src/main/java/edu/spring/step2/controller/UserController.java
@@ -4,14 +4,16 @@ import edu.spring.step2.model.User;
 import edu.spring.step2.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
 
 @Controller
-@RequestMapping(name = "/users")
+@RequestMapping("/users")
 public class UserController {
 
     @Autowired
@@ -21,6 +23,27 @@ public class UserController {
     @RequestMapping(value = "/list", method = RequestMethod.GET)
     public List<User> getAllUsers() {
         return userService.getAllUsers();
+    }
+
+    @ResponseBody
+    @RequestMapping(value = "/check", method = RequestMethod.POST)
+    public String check(@ModelAttribute("userFromServer") User user) {
+
+        for ( User u : userService.getAllUsers()) {
+            if (u.getName().equals(user.getName()) && u.getPassword().equals(user.getPassword())) {
+                return "Valid";
+            }
+        }
+
+        return "Invalid";
+    }
+
+    @RequestMapping(value = "/validate", method = RequestMethod.GET)
+    public ModelAndView validateUser() {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.addObject("userFromServer", new User());
+        modelAndView.setViewName("users_check_page");
+        return modelAndView;
     }
 
 }
